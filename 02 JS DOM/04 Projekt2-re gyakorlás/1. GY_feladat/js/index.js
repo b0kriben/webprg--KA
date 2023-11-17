3e1e0e2d4f600907eeda5c586c24891b2290a3d7
Krisztin Benedek <krisztinbeni@gmail.com>
2023-11-17 11:23:39 +0100

    commit

---
+++ 02 JS DOM/04 Projekt2-re gyakorlás/1. GY_feladat/js/index.js
@@ -0,0 +1,17 @@
+var user = [firstName, lastName]
+
+function userName()
+{
+    let firstName = "John";
+    let lastName = "Smith";
+    const user = {firstName:"John", lastName:"Smith"}
+
+    document.getElementById("kimenet").value;
+}
+
+function torol(){
+    let index = document.getElementById(0).value;
+    let ujFirstName = document.getElementById("ujname").value;
+
+    tomb.splice(index, 1, ujFirstName);
+}
