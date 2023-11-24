92b0fc2b134cc35b00662bce81e87749fc98b88f
Krisztin Benedek <krisztinbeni@gmail.com>
2023-11-24 11:12:45 +0100

    3.f kesz

--- 02 JS DOM/04 Projekt2-re gyakorlás/3. GY_feladat/js/script.js
+++ 02 JS DOM/04 Projekt2-re gyakorlás/3. GY_feladat/js/script.js
@@ -1,4 +1,25 @@
-const person1 = {fname:"Anna : ", penz:2100}
+let fname1 = "Anna";
+let penz1 = 2100;
+let fname2 = "Cecil";
+let penz2 = 1890;
+let fname3 = "Emil";
+let penz3 = 2050;
+let fname4 = "Gerald";
+let penz4 = 2920;
+
+const fizetes = {
+  fname1:"Anna", penz1:2100, 
+  fname2:"Cecil", penz2:1890, 
+  fname3:"Emil", penz3:2050, 
+  fname4:"Gerald", penz4:2920
+}
+
+document.getElementById("kereset").innerHTML = fizetes.fname1+" : "+fizetes.penz1+"<br>"+ fizetes.fname2+" : "+fizetes.penz2+"<br>"+ fizetes.fname3+" : "+fizetes.penz3+"<br>"+ fizetes.fname4+" : "+fizetes.penz4;
+
+
+
+
+/*const person1 = {fname:"Anna : ", penz:2100}
 const person2 = {fname:"Cecil : ", penz:1890}
 const person3 = {fname:"Emil : ", penz:2050}
 const person4 = { fname:"Gerald : ", penz:2920}
@@ -23,19 +44,16 @@ for (let x in person4) {
 document.getElementById("kereset").innerHTML = text1;
 document.getElementById("kereset").innerHTML = text2;
 document.getElementById("kereset").innerHTML = text3;
-document.getElementById("kereset").innerHTML = text4;
+document.getElementById("kereset").innerHTML = text4;*/
 
 
-/*let fizetes = { 
-    Anna : 2100, 
-    Cecil : 1890, 
-    Emil : 2050, 
-    Gerald : 2920 
+
+/*let text = "";
+for (let x in person) {
+  text += person[x];
 }
 
-const penz= {};
-
-document.getElementById("kereset").innerHTML = Anna.fizetes;*/
+document.getElementById("kereset").innerHTML = fizetes.Anna;*/
 
 /*let fizetes = {
 Anna : 2100,
