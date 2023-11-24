831ef28e5a0e8c4f69244f952b9a3d3ea0f61330
Krisztin Benedek <krisztinbeni@gmail.com>
2023-11-24 10:11:46 +0100

    2.f

--- 02 JS DOM/04 Projekt2-re gyakorlás/2. GY_feladat/js/script.js
+++ 02 JS DOM/04 Projekt2-re gyakorlás/2. GY_feladat/js/script.js
@@ -1,10 +1,23 @@
-/*Írd le az utasításokat, melyek a következőt valósítják meg:
+let firstName = "John";
+const user = {firstName:"John"};
+document.getElementById('name').innerHTML = user.firstName;
+
+function isEmpty()
+{
+    if(document.getElementById("name").innerHTML = null)
+    {
+        console.log(isEmpty({})) = true;
+    }
+    else
+    {
+        console.log(isEmpty({name: "John" })) = false;
+    }
+}
 
 
-1. Hozz létre egy üres user objektet
-2. Add a fistName property-t az obketumhoz a John értékkel.
-3. Add a lastName property-t az obketumhoz a Smith értékkel.
-4. Módisítsd a firstName-et Paul-ra.
-5. Töröld a firstName property-t az objektből.
+/*Hozz létre egy
 
-A konzolba írast ki a user objektet.*/
+isEmpty(obj) függvényt, mely true-t ad vissza, ha paraméterként adott objekt üres és false.t ad vissza, ha az objekt nem üres.
+
+console.log(isEmpty({})) --> true
+console.log(isEmpty({name: "John" })) --> false*/
