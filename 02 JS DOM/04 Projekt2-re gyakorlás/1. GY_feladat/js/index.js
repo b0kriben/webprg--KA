f52e823b1b2fb59954645e62e1c7f55aec96c4e5
Krisztin Benedek <krisztinbeni@gmail.com>
2023-11-24 09:41:02 +0100

    1. f

--- 02 JS DOM/04 Projekt2-re gyakorlás/1. GY_feladat/js/index.js
+++ 02 JS DOM/04 Projekt2-re gyakorlás/1. GY_feladat/js/index.js
@@ -1,20 +1,26 @@
-var user = [firstName, lastName]
+/*let firstName = "John";
+let lastName = "Smith";*/
+const user = {firstName:"John", lastName:"Smith"};
 
-function userName()
+/*let user = [firstName, lastName];*/
+
+//const user = {firstName:"John", lastName:"Smith"};
+document.getElementById('name').innerHTML = "Abraham Lincoln" //user[firstName] + user[lastName];
+
+/*function userName()
 {
-    let firstName = "John";
-    let lastName = "Smith";
     const user = {firstName:"John", lastName:"Smith"}
+    document.getElementById('user1').innerHTML = user.firstName;
 
     document.getElementById("kimenet").value;
 }
 
-function torol(){
+/*function torol(){
     let index = document.getElementById(0).value;
     let ujFirstName = document.getElementById("ujname").value;
 
     tomb.splice(index, 1, ujFirstName);
-}
+}*/
 
 /*Írd le az utasításokat, melyek a következőt valósítják meg:
 
--- 02 JS DOM/04 Projekt2-re gyakorlás/Data attributum/js/2x2.js
+++ 02 JS DOM/04 Projekt2-re gyakorlás/Data attributum/js/2x2.js
@@ -4,5 +4,6 @@ let index = document.getElementById("item").dataset.indexNumber
 
 function changeColor(this)
 {
-
+    document.getElementById("item").style.backgroundColor='red';
+    document.getElementById("item").style.backgroundColor='blue';
 }
