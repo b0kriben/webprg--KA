f645f1fb0471544e7a900fc0af89a0dac783c86b
Krisztin Benedek <krisztinbeni@gmail.com>
2023-12-11 07:52:40 +0100

    events

--- 02 JS DOM/04 Projekt2-re gyakorlás/Event handler és event listener/Feladat/js/eventsFeladat.js
+++ 02 JS DOM/04 Projekt2-re gyakorlás/Event handler és event listener/Feladat/js/eventsFeladat.js
@@ -1,7 +1,9 @@
-var tomb = ["text"];
+let button = document.querySelector(".btn");
 
-const button = document.querySelector(".btn")
+button.addEventListener("click",()=>
+{
+    let szoveg = document.querySelector("#szoveg").value;
+    console.log(szoveg)
 
-button.addEventListener("click",function() {
-console.log("text")
-})
+}    
+)
