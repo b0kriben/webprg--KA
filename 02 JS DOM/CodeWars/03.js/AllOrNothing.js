ad06fbc92bb32d755953258bafe345c85052b9b6
Krisztin Benedek <krisztinbeni@gmail.com>
2024-01-19 10:13:08 +0100

    allOrNothing folyamatban 01.19.

---
+++ 02 JS DOM/CodeWars/03.js/AllOrNothing.js
@@ -0,0 +1,8 @@
+function possiblyPerfect(key,answers) {
+    let key = ["F", "T", "T", "F", "F"];
+    let answer = ["F", "_", "T", "F", "T"];
+    if (answer == true){
+      console.log(answer);
+    }
+    return true || false ;
+  }
