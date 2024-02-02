c267e917db14a5c407338edca4fce93967f8ce0c
Krisztin Benedek <krisztinbeni@gmail.com>
2024-02-02 11:31:48 +0100

    javítások

--- 02 JS DOM/CodeWars/08_MaximumProduct.js/MaximumProduct.js
+++ 02 JS DOM/CodeWars/08_MaximumProduct.js/MaximumProduct.js
@@ -1,7 +1,7 @@
 function adjacentElementsProduct(array) {
-    let max = array[0] * array[1]
+    let max = array[0] * array[1];
     for (let i = 1; i < array.length - 1; i++) {
-      max = Math.max(max, array[i] * array[i + 1])
+      max = Math.max(max, array[i] * array[i + 1]);
     }
-    return max
+    return max;
 }
