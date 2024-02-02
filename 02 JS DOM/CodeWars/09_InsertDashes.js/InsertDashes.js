6412d68c4b3785610bfce5ec2b76da14c7d9bdae
Krisztin Benedek <krisztinbeni@gmail.com>
2024-02-02 11:30:47 +0100

    insertDashes kész

--- 02 JS DOM/CodeWars/09_InsertDashes.js/InsertDashes.js
+++ 02 JS DOM/CodeWars/09_InsertDashes.js/InsertDashes.js
@@ -1,11 +1,11 @@
-function insertDash(num) {
-    num = num.toString().split('')
-    
-    for(let i = 0; i < num.length; i++){
-      if (num[i]%2 > 0 && num[i +1]%2 > 0){
-        num[i] = num[i] + '-'
-      }
+function insertDash(number) {
+  number = number.toString().split('')
+  
+  for(let i = 0; i < number.length; i++){
+    if (number[i]%2 > 0 && number[i +1]%2 > 0){
+      number[i] = number[i] + '-';
     }
-   
-    return num.join('')
+  }
+ 
+  return number.join('');
 }
