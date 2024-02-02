2327852ef630e1a257d9ed2ba62dfbc15929d72d
Krisztin Benedek <krisztinbeni@gmail.com>
2024-02-02 09:48:55 +0100

    07 kész

--- 02 JS DOM/CodeWars/07_MeanVsMedian.js/MeanVsMedian.js
+++ 02 JS DOM/CodeWars/07_MeanVsMedian.js/MeanVsMedian.js
@@ -1,5 +1,5 @@
-function meanVsMedian(numbers) {
-    const mean = numbers.reduce((a, x, n) => a + (x - a) / ++n);
-    const median = numbers.sort((a, b) => a - b)[0 | numbers.length / 2];
-    return mean != median ? mean > median ? 'mean' : 'median' : 'same';
-  }  
+function meanVsMedian(numbers){
+  const mean = numbers.reduce((a, b, n) => a + (b-a)/ ++n);
+  const median = numbers.sort((a, c) => a - c)[0 | numbers.length / 2];
+  return mean != median ? mean > median ? 'mean' : 'median' : 'same';
+}
