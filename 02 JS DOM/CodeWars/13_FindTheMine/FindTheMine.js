c18bce945492a53c3df05fc7efaa11526579fa17
Krisztin Benedek <krisztinbeni@gmail.com>
2024-02-09 11:32:44 +0100

    13.f folyamatban

---
+++ 02 JS DOM/CodeWars/13_FindTheMine/FindTheMine.js
@@ -0,0 +1,13 @@
+function mineLocation(field){
+    let tomb = [];
+    
+    for(i=0; i=field; i++)
+    {
+      if(tomb[i] == 1)
+        {
+          tomb[];
+        }
+    }
+    
+    return tomb[];
+  }
