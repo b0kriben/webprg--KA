bc3f4b2e103937ea8cf2aefae638283c5ba46e63
Krisztin Benedek <krisztinbeni@gmail.com>
2024-02-23 12:53:41 +0100

    14.feladat codewars nem működik

--- 02 JS DOM/CodeWars/14_SortArrays(IgnoringCase)/SortArrays(IgnoringCase).js
+++ 02 JS DOM/CodeWars/14_SortArrays(IgnoringCase)/SortArrays(IgnoringCase).js
@@ -1,7 +1,21 @@
 // input: names - unsorted strings
 // output: case-agnostic sort
-sortme = function( names ){
+/*sortme = function( names ){
   
   
     return names;
-}  
+}*/  
+
+sortme = function(names){
+
+    let tomb=[];
+
+    if(tomb[0]/2 == 1)
+    {
+        names == tomb[0];
+        tomb[0] == tomb[1];
+        tomb[1] == names;
+    }
+    
+    return names;
+}
