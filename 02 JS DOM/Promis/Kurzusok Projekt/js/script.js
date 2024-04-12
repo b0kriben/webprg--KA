1548e6d423af11f79690d51bd3619e0f7133d9b1
Krisztin Benedek <krisztinbeni@gmail.com>
2024-04-12 11:23:15 +0200

    kurzusok projekt folyamatban

--- 02 JS DOM/Promis/Kurzusok Projekt/js/script.js
+++ 02 JS DOM/Promis/Kurzusok Projekt/js/script.js
@@ -11,66 +11,153 @@ fetch(url)
         .catch(error => console.log("Hiba történt: " + error))
 
 // Kurzus
-function search(params) {
-    cityName = document.getElementById("id").value
-    fetch('https://hur.webmania.cc/zips/'+ cityName+'.json')
+// lekérés
+fetch("https://jsonplaceholder.typicode.com/users&quot" {
+method: "GET"
+})
     .then(response => response.json())
-    .then(zips => {
-      console.log(zips);
-      kiiras(zips)
-    })
-    .catch(error => {
-    document.getElementById("kimenet").innerHTML="Nincs a keresésnek megfelelő találat"  
-    });
-  }
-  
-  function kiiras(zips) {
-    let ki = document.getElementById("kimenet")
-    ki.innerHTML = "";
-    let city = []
-    for (let i = 0; i < zips.zips.length; i++) {
-        if (city.includes(zips.zips[i].name)) {
-            continue
-        }
-        ki.innerHTML+=zips.zips[i].zip + ": "
-        ki.innerHTML += zips.zips[i].name + "\n<br>"
-        varosok.push(zips.zips[i].name)  
+    .then(json => {
+        let li = `<tr><th>Name</th><th>Email</th></tr>`;
+        json.forEach(user => {
+            li += `<tr>
+                <td>${user.name} </td>
+                <td>${user.email}</td>        
+            </tr>`;
+        });
+    document.getElementById("users").innerHTML = li;
+});
+
+
+
+//hozzáadás
+fetch("https://jsonplaceholder.typicode.com/posts/1&quot;" {
+     
+    // Metódus hozzáadása
+    method: "PUT",
+     
+    // Küldendő test vagy tartalom hozzáadása
+    body: JSON.stringify({
+        id: 1,
+        title: "foo",
+        body: "bar",
+        userId: 1
+    }),
+     
+    // Fejlécek hozzáadása a kéréshez
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
     }
-  
-    document.getElementById("lab").style.display="none";
-    ki.style.height = "max-content"
-    ki.style.width = "max-content"
-  }
+})
+ 
+// Konvertálás JSON-ba
+.then(response => response.json())
+ 
+// Az eredmények megjelenítése a konzolon
+.then(json => console.log(json));
+
+
+
+// törlés
+fetch("https://jsonplaceholder.typicode.com/posts/1&quot;" {
+     
+    // Metódus  hozzáadása
+    method: "DELETE",
+     
+    // Fejlécek hozzáadása a kéréshez
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+ 
+// Konvertálás JSON-ba
+.then(response => {
+    // Ha a válasz státusza OK, akkor visszatérünk egy üres JSON objektummal
+    if (response.ok) {
+        return response.json();
+    }
+    // Ha a válasz státusza nem OK, akkor dobunk egy hibát
+    throw new Error('Network response was not ok.');
+})
+ 
+// Az eredmények megjelenítése a konzolon
+.then(json => console.log(json))
+// Hibakezelés
+.catch(error => console.error('There was a problem with the fetch operation:', error));
+
+
+
 
 
 //Diák
-function search(params) {
-    cityName = document.getElementById("id").value
-    fetch('https://hur.webmania.cc/zips/'+ cityName+'.json')
+// lekérés
+fetch("https://jsonplaceholder.typicode.com/users&quot" {
+method: "GET"
+})
     .then(response => response.json())
-    .then(zips => {
-      console.log(zips);
-      kiiras(zips)
-    })
-    .catch(error => {
-    document.getElementById("kimenet").innerHTML="Nincs a keresésnek megfelelő találat"  
-    });
-  }
-  
-  function kiiras(zips) {
-    let ki = document.getElementById("kimenet")
-    ki.innerHTML = "";
-    let city = []
-    for (let i = 0; i < zips.zips.length; i++) {
-        if (city.includes(zips.zips[i].name)) {
-            continue
-        }
-        ki.innerHTML+=zips.zips[i].zip + ": "
-        ki.innerHTML += zips.zips[i].name + "\n<br>"
-        varosok.push(zips.zips[i].name)  
+    .then(json => {
+        let li = `<tr><th>Name</th><th>Email</th></tr>`;
+        json.forEach(user => {
+            li += `<tr>
+                <td>${user.name} </td>
+                <td>${user.email}</td>        
+            </tr>`;
+        });
+    document.getElementById("users").innerHTML = li;
+});
+
+
+
+//hozzáadás
+fetch("https://jsonplaceholder.typicode.com/posts/1&quot;" {
+     
+    // Metódus hozzáadása
+    method: "PUT",
+     
+    // Küldendő test vagy tartalom hozzáadása
+    body: JSON.stringify({
+        id: 1,
+        title: "foo",
+        body: "bar",
+        userId: 1
+    }),
+     
+    // Fejlécek hozzáadása a kéréshez
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
     }
-  
-    document.getElementById("lab").style.display="none";
-    ki.style.height = "max-content"
-    ki.style.width = "max-content"
-  }
+})
+ 
+// Konvertálás JSON-ba
+.then(response => response.json())
+ 
+// Az eredmények megjelenítése a konzolon
+.then(json => console.log(json));
+
+
+
+// törlés
+fetch("https://jsonplaceholder.typicode.com/posts/1&quot;" {
+     
+    // Metódus  hozzáadása
+    method: "DELETE",
+     
+    // Fejlécek hozzáadása a kéréshez
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+ 
+// Konvertálás JSON-ba
+.then(response => {
+    // Ha a válasz státusza OK, akkor visszatérünk egy üres JSON objektummal
+    if (response.ok) {
+        return response.json();
+    }
+    // Ha a válasz státusza nem OK, akkor dobunk egy hibát
+    throw new Error('Network response was not ok.');
+})
+ 
+// Az eredmények megjelenítése a konzolon
+.then(json => console.log(json))
+// Hibakezelés
+.catch(error => console.error('There was a problem with the fetch operation:', error));
