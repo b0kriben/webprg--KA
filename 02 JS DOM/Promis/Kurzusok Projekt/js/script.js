729628b3c511d11188f9c25fb20b68ce9fdbbd40
Krisztin Benedek <krisztinbeni@gmail.com>
2024-04-12 09:39:41 +0200

    kurzus projekt folyamatban

---
+++ 02 JS DOM/Promis/Kurzusok Projekt/js/script.js
@@ -0,0 +1,76 @@
+const url = "https://vvri.pythonanywhere.com/api/courses&quot"
+
+fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data)
+                data.forEach(element => {
+                    console.log(element)
+            })
+        })
+        .catch(error => console.log("Hiba történt: " + error))
+
+// Kurzus
+function search(params) {
+    cityName = document.getElementById("id").value
+    fetch('https://hur.webmania.cc/zips/'+ cityName+'.json')
+    .then(response => response.json())
+    .then(zips => {
+      console.log(zips);
+      kiiras(zips)
+    })
+    .catch(error => {
+    document.getElementById("kimenet").innerHTML="Nincs a keresésnek megfelelő találat"  
+    });
+  }
+  
+  function kiiras(zips) {
+    let ki = document.getElementById("kimenet")
+    ki.innerHTML = "";
+    let city = []
+    for (let i = 0; i < zips.zips.length; i++) {
+        if (city.includes(zips.zips[i].name)) {
+            continue
+        }
+        ki.innerHTML+=zips.zips[i].zip + ": "
+        ki.innerHTML += zips.zips[i].name + "\n<br>"
+        varosok.push(zips.zips[i].name)  
+    }
+  
+    document.getElementById("lab").style.display="none";
+    ki.style.height = "max-content"
+    ki.style.width = "max-content"
+  }
+
+
+//Diák
+function search(params) {
+    cityName = document.getElementById("id").value
+    fetch('https://hur.webmania.cc/zips/'+ cityName+'.json')
+    .then(response => response.json())
+    .then(zips => {
+      console.log(zips);
+      kiiras(zips)
+    })
+    .catch(error => {
+    document.getElementById("kimenet").innerHTML="Nincs a keresésnek megfelelő találat"  
+    });
+  }
+  
+  function kiiras(zips) {
+    let ki = document.getElementById("kimenet")
+    ki.innerHTML = "";
+    let city = []
+    for (let i = 0; i < zips.zips.length; i++) {
+        if (city.includes(zips.zips[i].name)) {
+            continue
+        }
+        ki.innerHTML+=zips.zips[i].zip + ": "
+        ki.innerHTML += zips.zips[i].name + "\n<br>"
+        varosok.push(zips.zips[i].name)  
+    }
+  
+    document.getElementById("lab").style.display="none";
+    ki.style.height = "max-content"
+    ki.style.width = "max-content"
+  }
